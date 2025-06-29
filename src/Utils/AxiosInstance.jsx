da34d07b0757b147a70acbf20abb4c0d46d97dae
Reyshano <reynohimawan@gmail.com>
2025-06-29 20:57:56 +0700

    axios instance diganti link deploy backend lagi

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "https://backend-client-beta.vercel.app/", // alamat json-server
+  baseURL: "https://final-backend-client.vercel.app/", // alamat json-server
   headers: {
     "Content-Type": "application/json",
   },
