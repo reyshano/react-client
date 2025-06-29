8909b957465eba4beb58908b4a5ff28f412b57e7
Reyshano <reynohimawan@gmail.com>
2025-06-29 19:51:11 +0700

    axion instance

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:3001", // alamat json-server
+  baseURL: "https://react-client-five.vercel.app/", // alamat json-server
   headers: {
     "Content-Type": "application/json",
   },
